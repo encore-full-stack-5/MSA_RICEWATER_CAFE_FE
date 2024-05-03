af512a3b681743f9861199c6aef0981d84effde7
jaeyoon00 <qwero55@naver.com>
2024-05-03 18:20:49 +0900

    상단바 완성

--- src/app/Main/Cafemain/page.js
+++ src/app/Main/Cafemain/page.js
@@ -1,5 +1,47 @@
+import common from '/src/styles/common.module.css'
+import Image from "next/image";
+import naverlogo from '/src/assets/naverlogo.png'
+import searchImg from '/src/assets/searchImg.png'
+import homelogo from '/src/assets/homelogo.png'
+
 export default function Cafemain (){
-    return(
-        <div>안녕하세요 저는 김재윤입니다 </div>
+    return (
+        <>
+        <div className={common.mainheder}>
+            <div className={common.logo}>
+                <Image src={naverlogo} alt={"naverlogo"}/>
+                <a className={common.cafelogo}>카페</a>
+            </div>
+            <div className={common.search}>
+                <input type="text" placeholder="원하는 카페, 글을 찾아보세요" maxLength="255"/>
+                <button type="submit" aria-label="검색" className="btn_search">
+                    <Image src={searchImg} alt={"searchlogo"}/>
+                </button>
+            </div>
+        </div>
+            <br/>
+            <hr/>
+            <div className={common.mainpage}>
+                <div className={common.cafeCategory}>
+                    <div>
+                        <button className={common.homebutton}>
+                            <Image src={homelogo} alt={"homelogo"}/>
+                            <a className={common.cafehome}>카페홈</a>
+                        </button>
+                    </div>
+                    <hr/>
+                    <div className={common.cafehomepage}>
+                        <a>dd</a>
+                    </div>
+
+                    <div className={common.cafeloginpage}>
+                        <a>dd</a>
+                    </div>
+
+
+                </div>
+
+            </div>
+        </>
     )
 }
