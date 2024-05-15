9784ab15f18a2a7089b09b9e23ca2d255971efd6
ParkMiram <myurm@naver.com>
2024-05-15 20:15:14 +0900

    [fix]: login token 중간 커밋

--- src/app/main/login/page.js
+++ src/app/main/login/page.js
@@ -3,17 +3,23 @@ import { useSearchParams } from 'next/navigation';
 import {useEffect} from "react";
 import axios from "/src/api/axiosInstance";
 
-
 export default function login() {
     const searchParams = useSearchParams();
     const parseLogin = async () => {
-        const response = axios.post("/auth/login", null,
-            { headers: {
-                        'Authorization' : searchParams.get("tokenType") + ' ' + searchParams.get("token")}
-            });
-        console.log(response);
+        const token = searchParams.get("token");
+        const tokenType = searchParams.get("tokenType");
 
-        localStorage.setItem("token", response.data?.token);
+        try {
+            const headers = {
+                "Content-Type": "application/json",
+                "Authorization": `${tokenType} ${token}`
+            }
+            const response = await axios.post("/auth/login", null, { headers: headers });
+            console.log(response);
+            localStorage.setItem("token", response.data?.token);
+        } catch (e) {
+            console.log("ERROR:" + e);
+        }
     }
 
     useEffect(() => {
